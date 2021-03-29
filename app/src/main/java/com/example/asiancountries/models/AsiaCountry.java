82ad9175b22b480d83904eadb568455cfc2cdfed
Akansha Jain <akanshajn01@gmail.com>
2021-03-30 03:19:54 +0530

    Added interface for Network and DB classes

---
+++ app/src/main/java/com/example/asiancountries/models/AsiaCountry.java
@@ -0,0 +1,15 @@
+package com.example.asiancountries.models;
+
+import java.io.Serializable;
+
+public interface AsiaCountry extends Serializable {
+    String getName();
+    String getCapital();
+    String getFlag();
+    String getRegion();
+    String getSubregion();
+    String getPopulation();
+    String getBorders();
+    String getLanguages();
+}
+
